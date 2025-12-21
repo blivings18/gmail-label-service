3863ee8b86ac6dd0e3329ede24e0c6284c996ab8
blivings18 <blivingston18@gmail.com>
2025-12-20 17:53:24 -0700

    Initial commit

---
+++ src/main/java/com/blivings18/gmail/labelservice/GmailLabelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blivings18.gmail.labelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailLabelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmailLabelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blivingston/gmail_label_service/GmailLabelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blivingston.gmail_label_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailLabelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
